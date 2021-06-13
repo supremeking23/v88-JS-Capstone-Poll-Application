dd17be2854f6cd6ccf8fb67062dd59f4c7b6c20f
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-06-14 06:06:47 +0800

    start collecting data and answer poll question is now working

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const EXPRESS = require("express");
 const app = EXPRESS();
 const PORT = 9000;
 
+const server = app.listen(PORT, (req, res) => {
+	console.log(`server is listening to port ${PORT}`);
+});
+const io = require("socket.io")(server);
+
 let bodyParser = require("body-parser");
 let session = require("express-session");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,6 +20,22 @@ app.use(
 	})
 );
 
+io.on("connection", function (socket) {
+	console.log(`a user is connected`);
+
+	socket.on("start-collecting-data", function (data) {
+		console.log(data.message);
+
+		socket.broadcast.emit("start-collecting-data-response", { message: "start collecting data response" });
+	});
+
+	socket.on("disconnect", function () {
+		console.log(`a user is disconnected`);
+
+		// users = [];
+	});
+});
+
 // for image/js/css
 app.use(EXPRESS.static(__dirname + "/assets"));
 // This sets the location where express will look for the ejs views
@@ -24,5 +45,3 @@ app.set("view engine", "ejs");
 // use app.get method and pass it the base route '/' and a callback
 
 require("./routes.js")(app);
-
-app.listen(PORT);
